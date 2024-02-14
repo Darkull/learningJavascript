b71ffb83061575f3cc43db382a14cfad823f0396
alifhossainbits <alifhossain5577@gmail.com>
2024-02-15 04:49:55 +0600

    Default Parameters

--- spaghetti/Function/function.js
+++ spaghetti/Function/function.js
@@ -1,1 +1,31 @@
 "use strict";
+
+// ---------- Default Parameters ------------
+
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers // we can also use other parameters that were set before it
+) {
+  // ES6
+
+  // ES5
+  //   price = price || 199;
+  //   numPassengers = numPassengers || 1;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+
+  bookings.push(booking);
+};
+
+createBooking(123);
+console.log(bookings);
+
+createBooking("Boeing737", undefined, 100);
