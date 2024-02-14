5e77232d7f1c1fd34224afd21a7094172c16f0d4
alifhossainbits <alifhossain5577@gmail.com>
2024-02-15 02:11:35 +0600

    added link

--- spaghetti/Data-Structures-Operators/dataStructure.js
+++ spaghetti/Data-Structures-Operators/dataStructure.js
@@ -75,6 +75,13 @@ const restaurant = {
 };
 
 //  ---------------------- String methods practice ---------------------
+const flights =
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
 
 //  ---------------------- Coding Challenge #4 ---------------------
 document.body.append(document.createElement("textarea"));
@@ -91,8 +98,6 @@ document.querySelector("button").addEventListener("click", function () {
     console.log(`${output.padEnd(20, " ")} ${"✅".repeat(i + 1)}`);
   }
 });
-
-const str = "The quick brown fox jumps over the lazy dog.";
 
 //  ---------------------- Working with string #3 ---------------------
 // console.log("alif+is+a+very+nice+guy".split("+"));
