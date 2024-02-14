8acc8af4d7d706404f2e3ded244104ebb1cde352
alifhossainbits <alifhossain5577@gmail.com>
2024-02-14 07:35:10 +0600

    string complete #3

--- spaghetti/Data-Structures-Operators/dataStructure.js
+++ spaghetti/Data-Structures-Operators/dataStructure.js
@@ -89,21 +89,46 @@ const restaurant = {
 // const newName = ["Mr.", firstName, lastName.toUpperCase()].join("-");
 // console.log(newName);
 
-// name capitalizor
-const capitalizeName = function (name) {
-  const names = name.split(" ");
+// // name capitalizor
+// const capitalizeName = function (name) {
+//   const names = name.split(" ");
 
-  const nameUpper = [];
-  for (const x of names) {
-    // nameUpper.push(x[0].toUpperCase() + x.slice(1));
-    nameUpper.push(x.replace(x[0], x[0].toUpperCase()));
-  }
-  console.log(nameUpper.join(" "));
+//   const nameUpper = [];
+//   for (const x of names) {
+//     // nameUpper.push(x[0].toUpperCase() + x.slice(1));
+//     nameUpper.push(x.replace(x[0], x[0].toUpperCase()));
+//   }
+//   console.log(nameUpper);
+//   console.log(nameUpper.join(" "));
+// };
+
+// capitalizeName("alif hossain");
+// capitalizeName("mohammad bin kashem");
+
+// // padding
+// const message = "go to gate 23!";
+// console.log(message.padStart(25, "="));
+// console.log(message.padEnd(25, "="));
+// console.log(message.padStart(25, "=").padEnd(30, "0"));
+
+// credit card number
+const maskCreditCard = function (number) {
+  const str = String(number);
+  const last = str.slice(-4);
+  console.log(last.padStart(str.length, "*"));
 };
 
-capitalizeName("alif hossain");
-capitalizeName("mohammad bin kashem");
+maskCreditCard("555615635613561554561");
 
+// Repeat
+const message2 = "Bad weather... All Departues Delayed... ";
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} number of planes in line ${"✈️".repeat(n)}`);
+};
+planesInLine(5);
+planesInLine(3);
 //  ---------------------- Working with string #2 ---------------------
 // const airline = "Darku Air Bangladesh";
 // console.log(airline.toLowerCase());
