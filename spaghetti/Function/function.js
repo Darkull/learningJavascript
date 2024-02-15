0dfaf75a8616b1a6b34ea6ef06c3f0f477f1a309
alifhossainbits <alifhossain5577@gmail.com>
2024-02-16 05:09:09 +0600

    returning functions

--- spaghetti/Function/function.js
+++ spaghetti/Function/function.js
@@ -1,37 +1,57 @@
 "use strict";
 
+// -------- Functions Returning Functions  ---------
+// create a function that returns a new function
+// const greet = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting}, ${name}`);
+//   };
+// };
+
+// const greeterHey = greet("Hey"); // greeterHey is the value of greet. so it's also a function. and we can use it outside.
+
+// greeterHey("Alif"); // output: Hey, Alif
+
+// greet("Assalamu alaikum,")("Alif");
+// // we call greet function, after calling we have another returned function inside of it. so we can call it again.
+
+// rewriting using only arrow function
+const greet = (greet) => (name) => console.log(`${greet}, ${name}`);
+
+greet("hi")("alif");
+
 // --- Functions accepting callback functions  ---
 // oneWord replaces all the spaces in a word
-const oneWord = function (str) {
-  return str.replace(/ /g, "").toLowerCase();
-};
+// const oneWord = function (str) {
+//   return str.replace(/ /g, "").toLowerCase();
+// };
 
-const upperFirstWord = function (str) {
-  const [first, ...others] = str.split(" ");
-  return [first.toUpperCase(), ...others].join(" ");
-};
+// const upperFirstWord = function (str) {
+//   const [first, ...others] = str.split(" ");
+//   return [first.toUpperCase(), ...others].join(" ");
+// };
 
-upperFirstWord("hello my name is Alif Hossain");
+// upperFirstWord("hello my name is Alif Hossain");
 
-// higher order function
-const transformer = function (str, fn) {
-  console.log(`Original string: ${str}`);
-  console.log(`Transformer string: ${fn(str)}`);
-  console.log(`Transformed by: name property is ${fn.name}`);
-};
+// // higher order function
+// const transformer = function (str, fn) {
+//   console.log(`Original string: ${str}`);
+//   console.log(`Transformer string: ${fn(str)}`);
+//   console.log(`Transformed by: name property is ${fn.name}`);
+// };
 
-transformer("Hello guy how are you?", upperFirstWord); // upperFirstWord is an callback function. we call them later on the transformer function!
-transformer("Hello guy how are you?", oneWord);
+// transformer("Hello guy how are you?", upperFirstWord); // upperFirstWord is an callback function. we call them later on the transformer function!
+// transformer("Hello guy how are you?", oneWord);
 
-const danger = function () {
-  console.log("⚠️");
-};
+// const danger = function () {
+//   console.log("⚠️");
+// };
 
-document.body.addEventListener("click", danger); // here addEvenetListener is an higher order function while the danger is a callback function
+// document.body.addEventListener("click", danger); // here addEvenetListener is an higher order function while the danger is a callback function
 
 // js uses callback all the time
 
-["alif", "asif", "shamim", "ninja"].forEach(danger);
+// ["alif", "asif", "shamim", "ninja"].forEach(danger);
 
 // --- How passing arguments works Value Vs Reference ---
 // const flight = "LH234";
