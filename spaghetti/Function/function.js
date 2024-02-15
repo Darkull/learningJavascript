3ed9d0b6cf67c5accf4816637ea9730ffdbde71a
alifhossainbits <alifhossain5577@gmail.com>
2024-02-15 10:58:15 +0600

    higher order function, callback etc

--- spaghetti/Function/function.js
+++ spaghetti/Function/function.js
@@ -1,38 +1,70 @@
 "use strict";
 
+// --- Functions accepting callback functions  ---
+// oneWord replaces all the spaces in a word
+const oneWord = function (str) {
+  return str.replace(/ /g, "").toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), ...others].join(" ");
+};
+
+upperFirstWord("hello my name is Alif Hossain");
+
+// higher order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformer string: ${fn(str)}`);
+  console.log(`Transformed by: name property is ${fn.name}`);
+};
+
+transformer("Hello guy how are you?", upperFirstWord); // upperFirstWord is an callback function. we call them later on the transformer function!
+transformer("Hello guy how are you?", oneWord);
+
+const danger = function () {
+  console.log("⚠️");
+};
+
+document.body.addEventListener("click", danger); // here addEvenetListener is an higher order function while the danger is a callback function
+
+// js uses callback all the time
+
+["alif", "asif", "shamim", "ninja"].forEach(danger);
+
 // --- How passing arguments works Value Vs Reference ---
-const flight = "LH234";
-const alif = {
-  name: "Alif Hossain",
-  passport: 234343356,
-};
+// const flight = "LH234";
+// const alif = {
+//   name: "Alif Hossain",
+//   passport: 234343356,
+// };
 
-const checkIn = function (flightNum, passenger) {
-  flightNum = "LH999"; // name change didn't affect original flight variable.because its a primitive data type.
-  passenger.name = "Mr. " + passenger.name; // this has affected the original alif vairable
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = "LH999"; // name change didn't affect original flight variable.because its a primitive data type.
+//   passenger.name = "Mr. " + passenger.name; // this has affected the original alif vairable
 
-  if (passenger.passport === 234343356) {
-    alert("Checked in");
-  } else {
-    alert("Wrong Passport!");
-  }
-};
+//   if (passenger.passport === 234343356) {
+//     alert("Checked in");
+//   } else {
+//     alert("Wrong Passport!");
+//   }
+// };
 
-checkIn(flight, alif);
-console.log(flight);
-console.log(alif);
+// checkIn(flight, alif);
+// console.log(flight);
+// console.log(alif);
 
-// is the same as doing...
-const flightName = flight;
-const passenger = alif; // when we copy an object like this, we are really copying the reference to that object in the memory. They both point the same object in memory.
-console.log(Math.trunc(Math.random() * 10000000));
+// // is the same as doing...
+// const flightName = flight;
+// const passenger = alif; // when we copy an object like this, we are really copying the reference to that object in the memory. They both point the same object in memory.
 
-const newPassport = function (person) {
-  person.passport = Math.trunc(Math.random() * 10000000);
-};
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 10000000);
+// };
 
-newPassport(alif);
-checkIn(flight, alif);
+// newPassport(alif);
+// checkIn(flight, alif);
 
 // ---------- Default Parameters ------------
 
