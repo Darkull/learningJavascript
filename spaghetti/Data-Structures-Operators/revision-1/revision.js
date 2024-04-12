89a0912e0f735fec6e5d28037d89ef9fcc8a89d5
alifhossainbits <alifhossain5577@gmail.com>
2024-04-12 06:20:43 +0600

    .

--- spaghetti/Data-Structures-Operators/revision-1/revision.js
+++ spaghetti/Data-Structures-Operators/revision-1/revision.js
@@ -13,39 +13,74 @@ const restaurant =  {
     order: function(starterIndex, mainIndex) {
         return [this.starterMenu[starterIndex], this.starterMenu[mainIndex]]
     },
+    openingHours: {
+        thu: {
+          open: 12,
+          close: 22,
+        },
+        fri: {
+          open: 11,
+          close: 23,
+        },
+        sat: {
+          open: 0, // Open 24 hours
+          close: 24,
+        },
+      },
 };
+
+///////////////// Object DESTRUCTURING
+// const {name, location, categories, starterMenu, mainMenu, openingHours} = restaurant;
+// console.log(locations);
+
+const {name, starterMenu, openingHours} = restaurant;
+console.log(name, starterMenu, openingHours);
 
 
 ///////////////// ARRAY DESTRUCTURING
 
-console.log('test');
-console.log('test');
+// console.log('test');
+// console.log('test');
 
-const arr = [2, 3, 4];
-const a = arr[0];
-console.log(a);
+// const arr = [2, 3, 4];
+// const a = arr[0];
+// console.log(a);
 
 
-// destructuring assignment
-const [x, y, z] = arr;
-console.log(x, z);
-console.log(x, z);
+// // destructuring assignment
+// const [x, y, z] = arr;
+// console.log(x, z);
+// console.log(x, z);
 
-// switching variables
-let [first, ,third] = restaurant.categories;
-console.log(first, third);
-
-// normal
-// const temp = first;
-// first = third;
-// third = temp;
+// // switching variables
+// let [first, ,third] = restaurant.categories;
 // console.log(first, third);
 
-//best method
-[first, third] = [third, first]
-console.log(first, third);
+// // normal
+// // const temp = first;
+// // first = third;
+// // third = temp;
+// // console.log(first, third);
 
-// immediately destructuring an function with multiple variables
-console.log(restaurant.order(0, 2));
-const [starterMenuItem, mainMenuItem] = restaurant.order(0, 2);
-console.log(starterMenuItem, mainMenuItem);
+// //best method
+// [first, third] = [third, first]
+// console.log(first, third);
+
+// // immediately destructuring an function with multiple variables
+// console.log(restaurant.order(0, 2));
+// const [starterMenuItem, mainMenuItem] = restaurant.order(0, 2);
+// console.log(starterMenuItem, mainMenuItem);
+
+// // nester array (one array inside another )
+// const nested = ["a", "b", [3, 4]];
+// const [i, , j] = nested;
+// console.log(i, j);
+
+// // get the values inside the nested array as individual value?
+// const [aa, ab, [ac, ad]] = nested;
+// console.log(aa, ab, ac, ad);
+
+// // set defaults, how to not get undefined
+// const [p = 1, q = 1, r = 1] = [7, 8];
+// console.log(p, q, r);
+// // output: 7 8 undefined
